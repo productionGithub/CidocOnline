b0fbba121b983fb5108a88b72d8bbd81ac073636
Talgorn Francois-Xavier <fx.talgorn@indytion.com>
2022-06-10 11:16:51 +0200

    Change repo name

---
+++ Assets/Core/Scenes/Cards/EntityCard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// Entity card Object used to instanciate 'entityCards' Dictionary<int, EntityCard>
+/// See DecksController.
+/// </summary>
+
+public class EntityCard : InteractiveCard
+{
+    int i;
+}
